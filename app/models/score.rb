34e66b7cdb9f686be0579429f80164c94c3032cb
jmxu94 <jmxu94@gmail.com>
2017-05-09 16:26:58 -0700

    Remove cornman api skeleton

--- app/models/score.rb
+++
@@ -1,6 +0,0 @@
-class Score < ApplicationRecord
-  # validates :username, :score, presence: true
-  # validates :username, length: { minimum: 1 }
-  # validates :username, length: { maximum: 10 }
-  validates :score, presence: true
-end
