a87ba77023b9b1275edab6a6e0640c8dc7f38b97
jmxu94 <jmxu94@gmail.com>
2017-06-07 15:50:54 -0700

    Create transparent clickable image flowers

--- main.js
+++ main.js
@@ -1,8 +1,20 @@
-$(window).on('hashchange', function() {
-   $("#nav a").each(function() {   
-      if (this.href == window.location.href) {
-          $("#nav a").removeClass("icon-active");
-          $(this).addClass("icon-active");
-      }
+$(document).ready(function() {
+
+  $(window).on('hashchange', function() {
+     $("#nav a").each(function() {   
+        if (this.href == window.location.href) {
+            $("#nav a").removeClass("icon-active");
+            $(this).addClass("icon-active");
+        }
+    });
   });
+
+
+$('.timeline').on('click', 'img', function() {
+  alert("hi");
 });
+
+
+
+})
+
