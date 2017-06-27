e7e2c9b25d3431d4a2f1c5e64b59631ba37ee787
jmxu94 <jmxu94@gmail.com>
2017-06-26 23:38:05 -0700

    Add to timeline page and add name to each page

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 $(document).ready(function() {
 
   $(window).on('hashchange', function() {
-     $("#nav a").each(function() {   
+     $("#nav a").each(function() {
         if (this.href == window.location.href) {
             $("#nav a").removeClass("icon-active");
             $(this).addClass("icon-active");
@@ -11,12 +11,14 @@ $(document).ready(function() {
 
   $('.event-content').first().css("display", "unset");
 
-  $('.event-container').on('click', 'img', function() {
+  $('.event').on('click', 'img', function() {
     $('.event-content').css("display", "none");
+    $('.event img').css("opacity", "0.5");
+
+    $(this).css("opacity", "1");
     var ecNum = $('.event-container img').index(this);
 
     $('#ec' + ecNum).css("display", "unset");
   });
 
 })
-
